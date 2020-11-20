a849046a10c6542eae1dc49c0426e1783d1f1ea7
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2020-11-19 20:09:42 -0500

    Add microsoft to ExtendedAdapterInfo

--- rend3/src/renderer/info.rs
+++ rend3/src/renderer/info.rs
@@ -5,6 +5,7 @@ pub enum Vendor {
     NV,
     AMD,
     Intel,
+    Microsoft,
     Broadcom,
     Qualcomm,
     Unknown(usize),
@@ -30,6 +31,7 @@ impl From<AdapterInfo> for ExtendedAdapterInfo {
             vendor: match info.vendor {
                 0x1002 => Vendor::AMD,
                 0x10DE => Vendor::NV,
+                0x1414 => Vendor::Microsoft,
                 0x14E4 => Vendor::Broadcom,
                 0x5143 => Vendor::Qualcomm,
                 0x8086 => Vendor::Intel,
@@ -44,6 +46,8 @@ impl From<AdapterInfo> for ExtendedAdapterInfo {
 impl ExtendedAdapterInfo {
     pub fn subgroup_size(&self) -> u32 {
         match self.vendor {
+            // TODO: warp warp
+            Vendor::Microsoft => 8,
             // TODO: buy a rpi4 and verify
             Vendor::Broadcom => 16,
             Vendor::Intel | Vendor::NV => 32,
