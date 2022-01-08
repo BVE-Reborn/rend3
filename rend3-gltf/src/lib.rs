cdc61d2f133a54ebf5e9f14a9513538cd157e820
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2022-01-08 12:01:30 -0500

    Reset values to sane amounts

--- rend3-gltf/src/lib.rs
+++ rend3-gltf/src/lib.rs
@@ -352,7 +352,7 @@ pub fn load_gltf_nodes<'a, E: std::error::Error + 'static>(
                         color: Vec3::from(light.color()),
                         intensity: light.intensity(),
                         direction,
-                        distance: 1000.0,
+                        distance: 100.0,
                     }))
                 }
                 _ => None,
--- rend3/src/lib.rs
+++ rend3/src/lib.rs
@@ -134,4 +134,4 @@ pub use surface::*;
 pub const INTERNAL_SHADOW_DEPTH_FORMAT: wgpu::TextureFormat = wgpu::TextureFormat::Depth32Float;
 // TODO: This needs to be dynamic
 /// Resolution of all shadow maps.
-pub const SHADOW_DIMENSIONS: u32 = 8192;
+pub const SHADOW_DIMENSIONS: u32 = 2048;
