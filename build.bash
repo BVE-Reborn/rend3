2d6c7f78792d1b675794f0fbd70753e1ebdab01f
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2023-07-16 17:24:57 -0400

    Fix webgpu build

--- build.bash
+++ build.bash
@@ -15,7 +15,7 @@ case $1 in
         RUSTFLAGS=--cfg=web_sys_unstable_apis cargo build --target wasm32-unknown-unknown $BUILD_FLAGS --bin $@
         mkdir -p target/generated/
         rm -rf target/generated/*
-        cp -r examples/$1/resources/ target/generated/ || true
+        cp -r examples/$1/resources target/generated/ || true
         sed "s/{{example}}/$1/g" > target/generated/index.html < examples/resources/index.html
         wasm-bindgen --out-dir target/generated --target web target/wasm32-unknown-unknown/$WASM_BUILD_DIR/$1.wasm
     ;;
