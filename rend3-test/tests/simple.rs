f39478fb976533f1a006d88bec44dcc454cf844d
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2023-07-24 19:31:55 -0400

    Add coordinate space test

--- rend3-test/tests/simple.rs
+++ rend3-test/tests/simple.rs
@@ -64,3 +64,65 @@ pub async fn triangle() -> anyhow::Result<()> {
 
     Ok(())
 }
+
+#[test_attr]
+pub async fn coordinate_space() -> anyhow::Result<()> {
+    let tests = [
+        // Right vector, up vector, camera vector
+        ("NegZ", Vec3::X, Vec3::Y, -Vec3::Z),
+        ("Z", -Vec3::X, Vec3::Y, Vec3::Z),
+        ("NegY", Vec3::X, -Vec3::Z, -Vec3::Y),
+        ("Y", Vec3::X, Vec3::Z, Vec3::Y),
+        ("NegX", -Vec3::Z, Vec3::Y, -Vec3::X),
+        ("X", Vec3::Z, Vec3::Y, Vec3::X),
+    ];
+
+    let iad = rend3::create_iad(None, None, None, None)
+        .await
+        .context("InstanceAdapterDevice creation failed")?;
+
+    let Ok(runner) = TestRunner::builder().iad(iad.clone()).handedness(Handedness::Left).build().await else {
+            return Ok(());
+        };
+
+    let _objects = tests.map(|(_name, right_vector, up_vector, camera_vector)| {
+        // Clockwise triangle
+        let mesh = MeshBuilder::new(
+            vec![
+                0.5 * right_vector + -0.5 * up_vector,
+                -0.5 * right_vector + -0.5 * up_vector,
+                0.0 * right_vector + 0.5 * up_vector,
+            ],
+            Handedness::Left,
+        )
+        .build()
+        .expect("Failed to create mesh");
+
+        let color = if camera_vector.is_negative_bitmask() != 0 {
+            camera_vector * -0.25
+        } else {
+            camera_vector
+        };
+
+        let mesh_hdl = runner.add_mesh(mesh);
+        let material_hdl = runner.add_color_material(color.extend(1.0));
+        let object = Object {
+            mesh_kind: ObjectMeshKind::Static(mesh_hdl),
+            material: material_hdl,
+            transform: Mat4::IDENTITY,
+        };
+        runner.add_object(object)
+    });
+
+    for (name, _right_vector, up_vector, camera_vector) in tests {
+        runner.set_camera_data(Camera {
+            projection: rend3::types::CameraProjection::Raw(Mat4::IDENTITY),
+            view: Mat4::look_at_lh(camera_vector, Vec3::ZERO, up_vector),
+        });
+
+        let file_name = format!("tests/results/coordinate-space-{name}.png");
+        runner.render_and_compare(64, file_name, 0.0).await?;
+    }
+
+    Ok(())
+}
