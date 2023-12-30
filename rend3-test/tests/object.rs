dcaa2f2fdda513561774852b0fa113fc2356fc70
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2023-12-29 20:49:47 -0500

    Add duplicate object test

--- rend3-test/tests/object.rs
+++ rend3-test/tests/object.rs
@@ -1,10 +1,68 @@
 use anyhow::Context;
-use glam::{Mat4, Vec3, Vec4};
+use glam::{Mat4, Quat, Vec3, Vec4};
 use rend3::{
-    types::{Camera, Handedness},
+    types::{Camera, Handedness, ObjectChange},
     util::freelist::FreelistDerivedBuffer,
 };
 use rend3_test::{no_gpu_return, test_attr, FrameRenderSettings, TestRunner, Threshold};
+
+/// Ensure that duplicate_object doesn't retain the object for an extra frame.
+#[test_attr]
+pub async fn deuplicate_object_retain() -> anyhow::Result<()> {
+    let iad = no_gpu_return!(rend3::create_iad(None, None, None, None).await)
+        .context("InstanceAdapterDevice creation failed")?;
+
+    let Ok(runner) = TestRunner::builder()
+        .iad(iad.clone())
+        .handedness(Handedness::Left)
+        .build()
+        .await
+    else {
+        return Ok(());
+    };
+
+    runner.set_camera_data(Camera {
+        projection: rend3::types::CameraProjection::Raw(Mat4::IDENTITY),
+        view: Mat4::IDENTITY,
+    });
+
+    let material = runner.add_unlit_material(Vec4::ONE);
+    let object1 = runner.plane(
+        material,
+        Mat4::from_scale_rotation_translation(Vec3::new(-0.25, 0.25, 0.25), Quat::IDENTITY, Vec3::new(-0.5, 0.0, 0.0)),
+    );
+
+    runner
+        .render_and_compare(
+            FrameRenderSettings::new(),
+            "tests/results/object/duplicate-object-retain-left.png",
+            Threshold::Mean(0.0),
+        )
+        .await?;
+
+    let _object2 = runner.duplicate_object(
+        &object1,
+        ObjectChange {
+            transform: Some(Mat4::from_scale_rotation_translation(
+                Vec3::new(-0.25, 0.25, 0.25),
+                Quat::IDENTITY,
+                Vec3::new(0.5, 0.0, 0.0),
+            )),
+            ..Default::default()
+        },
+    );
+    drop(object1);
+
+    runner
+        .render_and_compare(
+            FrameRenderSettings::new(),
+            "tests/results/object/duplicate-object-retain-right.png",
+            Threshold::Mean(0.0),
+        )
+        .await?;
+
+    Ok(())
+}
 
 /// There was a bug in the culling implementation where the per-material buffer
 /// was never resized to fit the number of objects in the scene once it was initially
